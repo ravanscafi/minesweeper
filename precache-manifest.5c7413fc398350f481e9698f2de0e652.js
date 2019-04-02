938929250cf0462c6e7ef6ac68445939865c294b
Ravan Scafi <rscafi@leroymerlin.com.br>
2019-04-02 12:02:25 -0300

    Updates

---
+++ precache-manifest.5c7413fc398350f481e9698f2de0e652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee5892389f7d854eaac0",
+    "url": "/minesweeper/static/css/main.7c9f0514.chunk.css"
+  },
+  {
+    "revision": "ee5892389f7d854eaac0",
+    "url": "/minesweeper/static/js/main.2fa4a267.chunk.js"
+  },
+  {
+    "revision": "0f86e2ffe97a36d3984a",
+    "url": "/minesweeper/static/js/runtime~main.dd26f679.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/minesweeper/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8c28558d6eb12652e8175832e116ce51",
+    "url": "/minesweeper/index.html"
+  }
+];
