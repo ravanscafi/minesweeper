f9777757f33dae8ecc8d40143ef86f653815cb95
Ravan Scafi <rscafi@gmail.com>
2019-04-03 01:32:22 -0300

    Enable SW and improve CSS for active squares

--- src/Square.js
+++ src/Square.js
@@ -4,7 +4,7 @@ import './Square.css';
 export default function Square(props) {
   return (
     <button
-      className={"Square"+(props.value === '*' ? ' mine' : (props.value !== null ? ' revealed number'+props.value : ''))}
+      className={'Square '+(props.value === '*' ? 'mine' : (props.value !== null ? 'revealed number'+props.value : 'unrevealed'))}
       onClick={props.onClick}
       onContextMenu={props.onRightClick}
     >
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './Game';
-// import * as serviceWorker from './serviceWorker';
+ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ serviceWorker.register();
